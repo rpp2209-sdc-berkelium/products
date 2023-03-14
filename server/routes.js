f0bc4a4144da541c02114b297170537acf828d2f
CJ Edgecombe <cjledgecombe@gmail.com>
2023-03-13 21:20:27 -0600

    added test routes and created server development script

--- server/routes.js
+++ server/routes.js
@@ -1,1 +1,34 @@
-// define different endpoint routes here
+const db = require('../db');
+const Router = require('express-promise-router');
+
+const router = new Router();
+
+// /products
+router.get('/', async (req, res) => {
+  console.log('/products route accessed');
+  res.sendStatus(200);
+})
+
+
+// /products/:product_id
+router.get('/:id', async (req, res) => {
+  console.log(`/products/:id route accessed, ${req.params.id}`);
+  res.sendStatus(200);
+})
+
+
+// /products/:product_id/styles
+router.get('/:id/styles', async (req, res) => {
+  console.log('/products/:id/styles route accessed');
+  res.sendStatus(200);
+})
+
+
+// /products/:product_id/related
+router.get('/:id/related', async (req, res) => {
+  console.log('/products/:id/related route accessed');
+  res.sendStatus(200);
+})
+
+// export these routes to be used as middleware in server/index.js
+module.exports = router;
