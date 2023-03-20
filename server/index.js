661e13c802d18a4de252aad7e333f97493706da6
CJ Edgecombe <cjledgecombe@gmail.com>
2023-03-20 17:48:11 -0600

    added cors middleware, adjusted styles route to accommodate graviton front-end

--- server/index.js
+++ server/index.js
@@ -1,10 +1,12 @@
 require('dotenv').config();
+const cors = require('cors');
 const express = require('express');
 const products = require('./routes');
 const port = process.env.SERVER_PORT || 8080;
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 
 // use the routes defined in server/routes.js
 app.use('/products', products);
