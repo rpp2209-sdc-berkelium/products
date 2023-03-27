edf32f4035e1bee6cc324adbce71d06de31048f1
CJ Edgecombe <cjledgecombe@gmail.com>
2023-03-26 21:27:18 -0600

    optimized postgres queries to rely less on the server

--- server/routes.js
+++ server/routes.js
@@ -5,7 +5,6 @@ const router = new Router();
 
 // /products
 router.get('/', async (req, res) => {
-  console.log('/products route accessed');
   const { page } = req.query;
   const { count } = req.query;
 
@@ -27,13 +26,11 @@ router.get('/', async (req, res) => {
 
 // /products/:product_id
 router.get('/:id', async (req, res) => {
-  console.log('/products/:id route accessed');
   const { id } = req.params;
 
   try {
-    const productInfo = await db.query('SELECT * FROM products WHERE id = $1;', [id]);
-    const featureInfo = await db.query('SELECT feature, value FROM features WHERE product_id = $1;', [id]);
-    productInfo.rows[0].features = featureInfo.rows;
+    const productInfo = await db.query("SELECT products.*, jsonb_agg(jsonb_build_object('feature', features.feature, 'value', features.value)) \
+    AS features FROM products LEFT JOIN features ON products.id = features.product_id WHERE products.id = $1 GROUP BY products.id;", [id]);
     res.status(200).send(productInfo.rows[0]);
   } catch (e) {
     console.log('/products/:id route error', e);
@@ -44,7 +41,6 @@ router.get('/:id', async (req, res) => {
 
 // /products/:product_id/styles
 router.get('/:id/styles', async (req, res) => {
-  console.log('/products/:id/styles route accessed');
   const { id } = req.params;
 
   // define sku formatting function
@@ -56,16 +52,23 @@ router.get('/:id/styles', async (req, res) => {
   }
 
   try {
-    const styleList = await db.query('SELECT s.style_id, s.name, s.sale_price, s.original_price, s.default_style AS "default?", \
-    (SELECT json_agg(p) AS photos FROM (SELECT photos.thumbnail_url, photos.url FROM photos WHERE photos.style_id = s.style_id) AS p), \
-    (SELECT json_agg(skus) AS skus FROM skus WHERE s.style_id = skus.style_id) FROM styles AS s WHERE product_id = $1;', [id]);
+
+    //===================== SAVE FOR LATER, MIGHT BE MORE EFFICIENT IN DEPLOYMENT ================================
+    // const styleList = await db.query('SELECT s.style_id, s.name, s.sale_price, s.original_price, s.default_style AS "default?", \
+    // (SELECT json_agg(p) AS photos FROM (SELECT photos.thumbnail_url, photos.url FROM photos WHERE photos.style_id = s.style_id) AS p), \
+    // (SELECT json_agg(skus) AS skus FROM skus WHERE s.style_id = skus.style_id) FROM styles AS s WHERE product_id = $1;', [id]);
 
     // if skus exist for any of the styles, apply the sku formatting function to each array of skus in the style list
-    for (let style of styleList.rows) {
-      if (style.skus) {
-      style.skus = formatSkus(style.skus);
-      }
-    }
+    // for (let style of styleList.rows) {
+    //   if (style.skus) {
+    //   style.skus = formatSkus(style.skus);
+    //   }
+    // }
+
+    const styleList = await db.query(`SELECT s.style_id, s.name, s.sale_price, s.original_price, s.default_style AS "default?", \
+    (SELECT jsonb_agg(p) AS photos FROM (SELECT photos.thumbnail_url, photos.url FROM photos WHERE photos.style_id = s.style_id) AS p), \
+    jsonb_object_agg(skus.sku_id, (jsonb_build_object('quantity', skus.quantity, 'size', skus.size, 'sku_id', skus.sku_id))) AS skus FROM styles AS s LEFT JOIN skus \
+    ON s.style_id = skus.style_id WHERE product_id = $1 GROUP BY s.style_id;`, [id]);
 
     // create a payload with a 'results' key. The client is looking for this 'results' property, the front-end will break without it
     const payload = {
@@ -82,18 +85,12 @@ router.get('/:id/styles', async (req, res) => {
 
 // /products/:product_id/related
 router.get('/:id/related', async (req, res) => {
-  console.log('/products/:id/related route accessed');
   const { id } = req.params;
 
   try {
-    const related = await db.query('SELECT related_product_id FROM related WHERE related.current_product_id = $1;', [id]);
+    const related = await db.query('SELECT array_agg(related.related_product_id) FROM related WHERE related.current_product_id = $1;', [id]);
 
-    // format the query result to be an array of related product id's
-    const relatedIDList = related.rows.map((obj) => {
-      return obj.related_product_id;
-    })
-
-    res.status(200).send(relatedIDList);
+    res.status(200).send(related.rows[0].array_agg);
   } catch (e) {
     console.log('products/:id/related route error', e);
     res.sendStatus(500);
